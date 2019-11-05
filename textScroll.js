df8f284a16dd0a54ecd7c5a038fd147138301ca5
jone <jone@cngold.com.cn>
2019-11-05 16:25:07 +0800

    修改说明

--- textScroll.js
+++ textScroll.js
@@ -7,8 +7,8 @@
 /* 
 	基于jquery的跑马灯插件，使用时先引入jquery插件
 	用法：$('xx').textScroll({trigger:'mouseenter',speed:30});
-	参数为一个对象，可缺省，trigger是一个字符串为触发方式，默认自动滚动，鼠标移入停止，可设置为mouseenter，此时为鼠标移入滚动，移出停止
-	speed表示滚动速度，为数字
+	参数为一个对象，可缺省，trigger是一个字符串为触发方式，默认值为auto表示自动滚动，鼠标移入停止，可设置为mouseenter，此时为鼠标移入滚动，移出停止
+	speed为数字表示滚动速度
 */
 
 $.fn.textScroll = function(options){
